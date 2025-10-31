e282cd8bd754925a6af4703e0aee5149e06650d0
CanseFr <jucansell@gmail.com>
2025-10-31 12:37:11 +0100

    :white_check_mark: Refacto test without id in construct

--- src/test/java/com/canse/product/ProductApplicationTests.java
+++ src/test/java/com/canse/product/ProductApplicationTests.java
@@ -1,13 +1,21 @@
 package com.canse.product;
 
+import com.canse.product.entities.Product;
+import com.canse.product.repos.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class ProductApplicationTests {
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
-    void contextLoads() {
+    public void testCreateProduct(){
+        Product product = new Product("Pc Dell",2250.00, new Date());
+        productRepository.save(product);
     }
-
 }
