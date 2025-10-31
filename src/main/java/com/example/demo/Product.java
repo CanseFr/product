ad733b289badb1008ad49a75c853a65cb482efc8
CanseFr <jucansell@gmail.com>
2025-10-31 10:37:52 +0100

    :sparkles: Create Product class

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long idProduct;
+    private String nameProduct;
+    private Date dateCreated;
+
+    public Product() {
+        super();
+    }
+
+    public Product(Long idProduct, String nameProduct, Date dateCreated) {
+        super();
+        this.idProduct = idProduct;
+        this.nameProduct = nameProduct;
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "idProduct=" + idProduct +
+                ", nameProduct='" + nameProduct + '\'' +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
+}
