8d47d6697640c562f203bd2f241836049e7ccb3e
CanseFr <jucansell@gmail.com>
2025-10-31 12:45:05 +0100

    :white_check_mark: Create unit test for product TDD

--- src/test/java/com/canse/product/ProductApplicationTests.java
+++ src/test/java/com/canse/product/ProductApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class ProductApplicationTests {
@@ -14,8 +15,37 @@ class ProductApplicationTests {
     private ProductRepository productRepository;
 
     @Test
-    public void testCreateProduct(){
-        Product product = new Product("Pc Dell",2250.00, new Date());
+    public void testCreateProduct() {
+        Product product = new Product("Pc Dell", 2250.00, new Date());
         productRepository.save(product);
     }
+
+    @Test
+    public void testFindProductById() {
+        Product product = productRepository.findById(1L).get();
+        System.out.println(product);
+    }
+
+    @Test
+    public void testUpdateProduct() {
+        Product product = productRepository.findById(1L).get();
+        product.setPriceProduct(2000.0);
+        productRepository.save(product);
+        System.out.println(product);
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        productRepository.deleteById(1L);
+    }
+
+    @Test
+    public void testFindAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
 }
