0f2c0ecd9aecc8e57391da11beca0e126b557cd4
canse <jucansell@gmail.com>
2025-11-02 21:17:26 +0100

    :recycle: Create new methosd in interface with auto detection attribute by jpa

--- doc/todo.md
+++ doc/todo.md
@@ -1,3 +1,4 @@
 
 Migrer test vers h2
 @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+Implementation de l'interface pour la recherche auto selon les attributs de la class
--- src/main/java/com/canse/product/repos/ProductRepository.java
+++ src/main/java/com/canse/product/repos/ProductRepository.java
@@ -3,5 +3,11 @@ package com.canse.product.repos;
 import com.canse.product.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByNameProduct(String nameProduct);
+    List<Product> findByNameProductContains(String nameProduct);
+
 }
