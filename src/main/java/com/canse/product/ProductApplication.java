c716eaa6f6abffacfcef5e2a8cbee3e844d55388
canse <jucansell@gmail.com>
2025-11-07 10:19:26 +0100

    :seedling: Create a quick seed in main

--- src/main/java/com/canse/product/ProductApplication.java
+++ src/main/java/com/canse/product/ProductApplication.java
@@ -2,17 +2,26 @@ package com.canse.product;
 
 import com.canse.product.entities.Category;
 import com.canse.product.entities.Product;
+import com.canse.product.entities.Role;
+import com.canse.product.entities.User;
+import com.canse.product.services.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProductApplication implements CommandLineRunner {
 
     @Autowired
     private RepositoryRestConfiguration repositoryRestConfiguration;
+
+    @Autowired
+    UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
@@ -21,6 +30,23 @@ public class ProductApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         repositoryRestConfiguration.exposeIdsFor(Product.class, Category.class);
+    }
+
+//    @PostConstruct
+//    void init() {
+//        userService.addRole(new Role(null,"ADMIN"));
+//        userService.addRole(new Role(null,"User"));
+//
+//        userService.saveUser(new User(null, "admin","admin",true, null));
+//        userService.saveUser(new User(null, "canse","admin",true, null));
+//
+//        userService.addRoleToUser("admin", "ADMIN");
+//        userService.addRoleToUser("canse", "USER");
+//    }
+
+    @Bean
+    BCryptPasswordEncoder getBeanBCrypt() {
+        return new BCryptPasswordEncoder();
     }
 }
 
