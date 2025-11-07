949bc37eee4e265cc7aac1c01c64ae22768f8ab8
canse <jucansell@gmail.com>
2025-11-07 09:53:54 +0100

    :sparkles: Create user and role with repos

---
+++ src/main/java/com/canse/product/entities/User.java
@@ -0,0 +1,28 @@
+package com.canse.product.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+}
---
+++ src/main/java/com/canse/product/repos/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.canse.product.repos;
+
+import com.canse.product.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
