84f9bc5a5047b408a6d3cdbc50ef80c6d8fe0771
canse <jucansell@gmail.com>
2025-11-07 09:41:36 +0100

    :recycle: Upgrade path api

--- src/main/java/com/canse/product/controllers/CategoryRestController.java
+++ src/main/java/com/canse/product/controllers/CategoryRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cat")
+@RequestMapping("/cat")
 @CrossOrigin()
 public class CategoryRestController {
 
