fbe4c5969d08573fb43a873cadada21d36fc0d90
canse <jucansell@gmail.com>
2025-11-12 16:36:14 +0100

    :lock: Create stoken utils

---
+++ src/main/java/com/canse/product/repos/VerificationTokenRepository.java
@@ -0,0 +1,8 @@
+package com.canse.product.repos;
+
+import com.canse.product.entities.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
+}
