d7c3531e8d057e08521cde45f3dab1a3b7b8a97f
canse <jucansell@gmail.com>
2025-11-13 15:58:58 +0100

    :sparkles: Implement disable account logique

--- src/main/java/com/canse/product/security/JWTAuthentication.java
+++ src/main/java/com/canse/product/security/JWTAuthentication.java
@@ -9,15 +9,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class JWTAuthentication extends UsernamePasswordAuthenticationFilter {
 
@@ -58,4 +58,27 @@ public class JWTAuthentication extends UsernamePasswordAuthenticationFilter {
 
         response.addHeader("Authorization", "Bearer " + jwt);
     }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+        if(failed instanceof DisabledException){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("application/json;charset=UTF-8");
+
+            Map<String,Object> data = new HashMap<>();
+            data.put("errorCause","disabled");
+            data.put("message","L'utilisateur est désactivé !");
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(data);
+            PrintWriter writer = response.getWriter();
+            writer.println(json);
+            writer.flush();
+
+        }else {
+            super.unsuccessfulAuthentication(request, response, failed);
+        }
+    }
+
 }
