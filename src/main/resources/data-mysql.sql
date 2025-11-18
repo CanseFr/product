a143c2af0f533fc13967444e4e8cacc8fe1b4426
canse <jucansell@gmail.com>
2025-11-18 11:13:35 +0100

    :rocket: Deploy backend 11

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -7,11 +7,11 @@ VALUES (2, 'USER')
 ON DUPLICATE KEY UPDATE name = 'USER';
 
 INSERT INTO user (id, username, password, enabled)
-VALUES (1, 'admin', 'admin', true)
+VALUES (1, 'admin', '$2a$10$c9pikaZBs3lSzcHaEgizpuTCaZFW2n2.dNFwsApLH7nNoBt1MsSq2', true)
 ON DUPLICATE KEY UPDATE username = 'admin';
 
 INSERT INTO user (id, username, password, enabled)
-VALUES (2, 'user', 'user', true)
+VALUES (2, 'user', '$2a$10$c9pikaZBs3lSzcHaEgizpuTCaZFW2n2.dNFwsApLH7nNoBt1MsSq2', true)
 ON DUPLICATE KEY UPDATE username = 'user';
 
 INSERT INTO user_role (user_id, role_id)
